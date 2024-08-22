0646c7f135e895a9970c5bbb903c5aeb08e99443
Vladimir Kashin <v.kashin@urent.ru>
2024-08-22 21:56:33 +0300

    Добавлена механика втыкания ножей в мишень (отвязка объекта от одного родителя и привязка к другому)

--- elements/knife/knife.gd
+++ elements/knife/knife.gd
@@ -8,7 +8,28 @@ var is_flying := false
 func _physics_process(delta: float) -> void:
 	if is_flying:
 		var collision = move_and_collide(Vector2.UP * SPEED * delta)
+		# Обрабатываем коллизию
+		if collision:
+			handle_collision(collision)
 	
 	
 func throw():
 	is_flying = true
+	
+	
+func handle_collision(collision: KinematicCollision2D):
+	var collider := collision.get_collider()
+	if collider is Target:
+		add_knife_to_target(collider)
+		is_flying = false
+		
+		
+func add_knife_to_target(target: Target):
+	# Отвязываем нож от родителя
+	get_parent().remove_child(self)
+	# Точка попадания
+	global_position = Target.KNIFE_POSITION
+	rotation = 0
+	# Привязка ножа к мишени, нож остаётся в ней
+	target.add_object_with_pivot(self, -target.rotation)
+		
--- elements/targets/target/target.gd
+++ elements/targets/target/target.gd
@@ -1,6 +1,18 @@
 extends CharacterBody2D
+class_name Target
+
+const KNIFE_POSITION = Vector2(0, 180)
 
 var speed := PI
 
+# Получим ссылку на item_containers
+@onready var item_containers := $ItemsContainer
+
 func _physics_process(delta: float) -> void:
 	rotation += speed * delta
+	
+func add_object_with_pivot(object: Node2D, object_rotation: float):
+	var pivot := Node2D.new()
+	pivot.rotation = object_rotation
+	pivot.add_child(object)
+	item_containers.add_child(pivot)
